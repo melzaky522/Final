d1a8d948434fa034b47588a9481373a6122c7be0
melzaky522 <melzaky522@gmail.com>
2017-12-20 21:05:24 -0500

    Coding is completed

--- tetrahymena_part_A_me1528.R
+++ tetrahymena_part_A_me1528.R
@@ -1,6 +1,8 @@
+
 #install.packages("tidyverse")
 #install.packages("dplyr")
 #install.packages("ggplot2")
+
 #load dplyr package
 library("dplyr")
 
@@ -11,23 +13,27 @@ library(ggplot2)
 library("tidyverse")
 library("readr")
 
-#load the script the tsv file
+#load the script the tsv file : Step 1
 mydata <- read_tsv("tetrahymena.tsv")
 View(mydata)
+
+#removing the rows which has diameter <= 19.2 : Step 2
 mydata <- mydata[!mydata$diameter <= 19.2,]
 
-#computing mean for culture level
+#computing mean for culture grouping : step 3 
 test1 <- mydata[,2:4] %>% group_by(culture) %>% summarise_all(funs(mean(., na.rm=TRUE)))
 
-#computing mean for glucose level
+#computing mean for glucose grouping : Step 3
 test2 <- mydata %>% group_by(glucose) %>% summarise_all(funs(mean(., na.rm=TRUE)))
 
-#adding new column for log_concentration 
+#adding new column for log_concentration  : Step 4
 mydata$log_conc <- log(mydata$conc)
 
-#adding new column for log_diameter
+#adding new column for log_diameter : Step 4
 mydata$log_diameter <- log(mydata$diameter)
 
-#doing scatter plot 
+#doing scatter plot  : Step 5 and Step 6
 ggplot(mydata, aes(x=log_conc, y=log_diameter,shape=glucose,color=glucose)) + geom_point() + geom_smooth(method=lm, se=FALSE, fullrange=TRUE)
-ggsave("etrahymena_part_A_NETID.pdf")
+
+#save the plot into PDF
+ggsave("tetrahymena_part_A_From_R_me1528.pdf")
